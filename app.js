6eda7eaad28f9ab74587d8832d303d34ede9502b
Selby Rowley-Cannon <selby@realrider.com>
2017-01-27 00:00:34 +0000

    Added Bootstrap and new font, implemented form, implemented basic messaging

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-// Server setup (serving files)
+// Require & server startup
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 
+// Serve the files to the client
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client', express.static(__dirname + '/client'));
 
+// Listen for connections on port 2000, callback console.log() for verification
 serv.listen(2000, console.log('Server ready'));
-
-// List of all connections to the server
-var SOCKET_LIST = {};
 
 // Socket comms (client <--> server)
 var io = require('socket.io')(serv, {});
 io.sockets.on('connection', function(socket) {
-	// Assign an id to the connection and add to the list
-	socket.id = Math.random();
-	SOCKET_LIST[socket.id] = socket;
 	// Run whenever a client connects
-	console.log('Socket connection: ' + socket.id);
+	console.log('Socket connected');
 
 	// Listen for messages
-	socket.on('happy', function() {
-		console.log("Bold always happy");
+	socket.on('message', function(data){
+		// Messages of type 'new-message' are sent to all users
+		io.sockets.emit('new-message', data);
+		// Messaged of type 'new-message' are sent to all users except the user who submitted the message
+		//socket.broadcast.emit('new-message', data);
 	});
 	
-	// Disconnect
+	// Run whenever a client disconnects
 	socket.on('disconnect', function() {
-		delete SOCKET_LIST[socket.id];
+		console.log('Socket disconnected');
 	});
 });
