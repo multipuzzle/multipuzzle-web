a4e046757070d777e0fe90a81970a98c63995589
Selby Rowley-Cannon <selby@realrider.com>
2017-01-27 13:44:41 +0000

    Added command detection

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 var serv = require('http').Server(app);
 
 // Keep track of users for unique nick validation
-nicknames = [];
+users = {};
 
 // Serve the files to the client
 app.get('/', function(req, res) {
@@ -23,8 +23,7 @@ io.sockets.on('connection', function(socket) {
 
 	// Listen for messages
 	socket.on('new-user', function(data, callback){
-		// All items not in an array have index == -1
-		if (nicknames.indexOf(data) != -1) {
+		if (data in users) {
 			// If it's already in the array, the new user can't have it
 			callback(false);
 		} else {
@@ -32,8 +31,8 @@ io.sockets.on('connection', function(socket) {
 			callback(true);
 			// Set arbitrary parameter "nickname" to the user's socket connection
  			socket.nickname = data;
- 			// Push it to the array
- 			nicknames.push(socket.nickname);
+ 			// Add it to the users object
+ 			users[socket.nickname] = socket;
  			// Send the new array to all users
  			updateNicknames();
 		}
@@ -41,12 +40,17 @@ io.sockets.on('connection', function(socket) {
 
 	// Send a list of all connected users' nicknames to all connected users
 	function updateNicknames() {
-		io.sockets.emit('usernames', nicknames);
+		io.sockets.emit('usernames', Object.keys(users));
 	}
 
 	socket.on('message', function(data){
-		// Messages of type 'new-message' are sent to all users
-		io.sockets.emit('new-message', {msg: data, nick: socket.nickname});
+		var msg = data.trim();
+		if (msg.substr(0, 3) === '/w ') { // Whisper (dm)
+			console.log('Whisper!');
+		} else { // Regular message
+			// Messages of type 'new-message' are sent to all users
+			io.sockets.emit('new-message', {msg: data, nick: socket.nickname});			
+		}
 		// Messaged of type 'new-message' are sent to all users except the user who submitted the message
 		//socket.broadcast.emit('new-message', data);
 	});
@@ -58,7 +62,7 @@ io.sockets.on('connection', function(socket) {
 			return;
 		} else {
 			console.log('Socket of ' + socket.nickname + ' disconnected');
-			nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+			delete users[socket.nickname];
 			updateNicknames();
 		}
 	});
